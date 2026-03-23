5b09ed87cdfd75e4094d9832fbddb92ee3f665b3
anasabbas10000000-sudo <anasabbas10000000@gmail.com>
2026-03-23 21:39:40 +0400

    app.js

--- app.js
+++ app.js
@@ -11,7 +11,9 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 // تحديد المجلد الذي يحتوي على ملفات الواجهة الأمامية
-app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname));
+
 app.use(express.json());
 
 // مسار ملفات البيانات
